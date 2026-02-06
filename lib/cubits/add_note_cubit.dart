f4a4e34e5d160bf4cbe17a6c84bcdaa121ba1006
maisath <thmysa919@gmail.com>
2026-02-06 03:24:31 +0300

    add add note cubit to add note bottom sheet

--- lib/cubits/add_note_cubit.dart
+++ lib/cubits/add_note_cubit.dart
@@ -8,14 +8,12 @@ part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
-
   addNote(NoteModel note) async {
     emit(AddNoteLoading());
     try {
       var notesBox = Hive.box<NoteModel>(
         kNotesBox,
       );
-
       emit(AddNoteSuccess());
       await notesBox.add(note);
     } catch (e) {
--- lib/views/widgets/AddNoteBottomSheet.dart
+++ lib/views/widgets/AddNoteBottomSheet.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:notes_app/constants.dart';
+import 'package:notes_app/cubits/add_note_cubit.dart';
+import 'package:notes_app/views/widgets/add_note_form.dart';
 import 'package:notes_app/views/widgets/custom_buttom.dart';
 import 'package:notes_app/views/widgets/custom_text_field.dart';
 
@@ -9,70 +13,35 @@ class AddNoteBottomSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(
+      padding: EdgeInsets.symmetric(
         horizontal: 16,
       ),
       child: SingleChildScrollView(
-        child: AddNoteForm(),
-      ),
-    );
-  }
-}
-
-class AddNoteForm extends StatefulWidget {
-  const AddNoteForm({super.key});
-
-  @override
-  State<AddNoteForm> createState() =>
-      _AddNoteFormState();
-}
-
-class _AddNoteFormState
-    extends State<AddNoteForm> {
-  final GlobalKey<FormState> formKey =
-      GlobalKey();
-
-  AutovalidateMode autovalidateMode =
-      AutovalidateMode.disabled;
-
-  String? title, subTitle;
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      autovalidateMode: autovalidateMode,
-      child: Column(
-        children: [
-          SizedBox(height: 35),
-          CustomTextField(
-            onSaved: (value) {
-              title = value;
-            },
-            hint: 'title',
-          ),
-
-          const SizedBox(height: 16),
-          CustomTextField(
-            onSaved: (value) {
-              subTitle = value;
-            },
-            hint: 'content',
-            maxLines: 5,
-          ),
-          const SizedBox(height: 40),
-          CustomButton(
-            onTap: () {
-              if (formKey.currentState!
-                  .validate()) {
-                formKey.currentState!.save();
-              } else {
-                autovalidateMode =
-                    AutovalidateMode.always;
-                setState(() {});
-              }
-            },
-          ),
-        ],
+        child:
+            BlocConsumer<
+              AddNoteCubit,
+              AddNoteState
+            >(
+              listener: (context, state) {
+                if (state is AddNoteFailure) {
+                  print(
+                    'failied ${state.errMessage}',
+                  );
+                }
+                if (state is AddNoteSuccess) {
+                  Navigator.pop(context);
+                }
+              },
+              builder: (context, state) {
+                return ModalProgressHUD(
+                  inAsyncCall:
+                      state is AddNoteLoading
+                      ? true
+                      : false,
+                  child: const AddNoteForm(),
+                );
+              },
+            ),
       ),
     );
   }
